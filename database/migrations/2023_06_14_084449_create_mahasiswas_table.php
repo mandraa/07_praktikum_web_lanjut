c41df99626dbd4649d99ee52b3b9d358424cdd31
mandraa <andra.arema87@gmail.com>
2023-06-14 20:02:44 +0700

    first commit

---
+++ database/migrations/2023_06_14_084449_create_mahasiswas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->id();
+            $table->string('Nim', 20)->unique();
+            $table->string('Nama', 50);
+            $table->string('Kelas', 15);
+            $table->string('Jurusan', 50);
+            $table->string('No_Handphone', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+};
